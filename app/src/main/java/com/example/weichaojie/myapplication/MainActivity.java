d78b3a7f7988585358cf219f163fea1afbdc5203
weichaojie <weichaojie@huawei.com>
2018-06-18 12:14:49 +0800

    新增linux下给系统用户授权的命令提示，linux系统下权限管理是一个需要注意的关键问题 Signed-off-by: weichaojie <weichaojie@huawei.com>
    Signed-off-by: weichaojie <weichaojie@huawei.com>

--- app/src/main/java/com/example/weichaojie/myapplication/MainActivity.java
+++ app/src/main/java/com/example/weichaojie/myapplication/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.weichaojie.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+
+// 在ubuntu上运行时需要添加授权
+// sudo chown weichaojie -R /dev/kvm
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: my first create a single app on android system!!!!!!!!");
     }
 }
